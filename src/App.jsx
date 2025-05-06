b834d15da6f1179bc62e690b67e733c7e1a544e1
partha-4444 <partha2031.be22@chitkara.edu.in>
2025-05-06 17:06:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div>
+        <h1>My react app</h1>
+        <h2>My react implemtation</h2>
+        <h3>Partha The Goat </h3>
+        <h1>New Updated Website </h1>
+        <h2>Updated Website</h2>
+       
+      </div>
+     
+    
+  )
+}
+
+export default App
